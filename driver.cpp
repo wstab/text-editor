9eaf4847d1fc4179b580dccb238d6c3537fb80a0
wstab <willstabile@gmail.com>
2025-05-11 12:55:02 -0400

    Create driver.cpp

---
+++ driver.cpp
@@ -0,0 +1,9 @@
+#include "TextEditor.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+  TextEditor T = TextEditor(); //Creates a new TextEditor object
+  T.Start(); //Calls the Start function
+  return 0;
+}
